f0e9247a8fd2d29b44b6a3b1ecdc7b53cb9ff74f
Himank <86199877+Himank-Khatri@users.noreply.github.com>
2023-08-05 16:40:26 +0530

    Add files via upload

---
+++ ohlcfetcher_v2.py
@@ -0,0 +1,93 @@
+from fyers_api import fyersModel, accessToken
+import webbrowser as wb
+import datetime as dt
+import os
+import pandas as pd
+import numpy as np
+
+
+app_id = "EFT30ANIEG-100"
+app_secret = "IIQ909PFQF"
+redirect_url = "https://www.google.com/"
+
+symbols = ["BPCL"]
+
+def login():
+    if not os.path.exists(f'{dt.date.today().strftime("%b-%d-%Y")}_access_token.txt'):
+        session = accessToken.SessionModel(client_id=app_id,
+                                           secret_key=app_secret,
+                                           redirect_uri=redirect_url,
+                                           response_type='code',
+                                           grant_type='authorization_code')
+        
+        response = session.generate_authcode()
+        wb.open(response)
+        auth_code=input("Enter auth code:")
+        session.set_token(auth_code)
+        access_token = session.generate_token()['access_token']
+        
+        with open(f'{dt.date.today().strftime("%b-%d-%Y")}_access_token.txt', 'w') as f:
+            f.write(access_token)
+        
+    else:
+        with open(f'{dt.date.today().strftime("%b-%d-%Y")}_access_token.txt', 'r') as f:
+            access_token = f.read().strip()
+        
+    return access_token
+
+access_token = login()
+fyers1 = fyersModel.FyersModel(client_id=app_id, token=access_token, log_path=os.getcwd())
+
+sdata = None
+df1 = None
+
+def get_data(symbol,res,start,end,date_format=1,cont=0):
+    global sdata
+    global df1
+    data = {'symbol':symbol, 'resolution':res, 'date_format':date_format, 
+            'range_from':start, 'range_to':end, 'cont_flag':cont}
+    
+    sdata = fyers1.history(data=data)
+    
+    df1 = pd.DataFrame(sdata['candles'])
+    df1[0]  = pd.to_datetime(df1[0], unit='s').dt.tz_localize('utc').dt.tz_convert('Asia/Kolkata')
+    df1.columns = ['date','open','high','low','close','volume']
+    
+    return df1
+    
+
+start_date = dt.date(2023,6,1)
+end_date =  dt.datetime.now().date()
+df = pd.DataFrame()
+
+total_days = abs((start_date-end_date).days)
+
+
+for symbol in symbols:
+    
+    start_date = dt.date(2023,6,1)
+    end_date =  dt.datetime.now().date()
+
+    df = pd.DataFrame()
+
+    total_days = abs((start_date-end_date).days)
+    
+    loop = None
+    print('fetching',symbol,'data')
+    while loop == None:
+        start_date = (end_date - dt.timedelta(days=total_days))
+        ed = (start_date + dt.timedelta(days = 99 if total_days>100 else total_days)).strftime("%Y-%m-%d")
+        start_date = start_date.strftime("%Y-%m-%d")
+        
+        new_data = get_data(f'NSE:{symbol}23AUGFUT',1,start_date,ed)
+        df = pd.concat([df,new_data])
+             
+        total_days = total_days-100 if total_days>100 else total_days-total_days
+        print(symbol,start_date,'to',ed,'data fetched')
+        if total_days == 0:
+            loop='done'
+            print('data fetched')
+            
+    
+    df.to_csv(f"{symbol}.csv")
+    print('Data Saved')
